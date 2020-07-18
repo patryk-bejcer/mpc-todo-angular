30bf1545a64d4dd434c6ff8435944eae349937af
patryk-bejcer <patryk.bejcer@gmail.com>
2020-07-18 10:06:19 +0200

    Clean default angular components and generate necessary components & services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo/todo-item/todo-item.component';
+import { TodoAddComponent } from './components/todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
